91804b891a1175eb397b6095f57abbc3721e620d
fidel pizarro <fidelpizarro11@gmail.com>
2024-08-20 22:04:38 -0300

    primer comit

---
+++ code-vite-app/src/components/navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+    
+    return (
+        <>
+<header className='header container' >
+    <nav className='navbar'>
+    <li className='navbar__link'><a href="">inicio</a></li>
+    <li className='navbar__link'><a href="">productos</a></li>
+    <li className='navbar__link'><a href="">contacto</a></li>
+    <li className='navbar__link'><a href="">carrito</a></li>
+    </nav>
+</header>
+</>
+)
+}
+
+
+export default Navbar;
